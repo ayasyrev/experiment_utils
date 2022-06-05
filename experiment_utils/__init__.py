5bc51f362b035733321e0360e08cfaef9da5064c
ayasyrev <a.yasyrev@gmail.com>
2022-06-05 11:32:58 +0300

    init, DwFlat

--- experiment_utils/__init__.py
+++ experiment_utils/__init__.py
@@ -1,1 +1,3 @@
 from experiment_utils.experiment import Experiment  # noqa F401
+from experiment_utils.utils.utils import show_cfg  # noqa F401
+from experiment_utils.utils.hydra_utils import read_config  # noqa F401
